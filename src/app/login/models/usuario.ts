32e0a24d5d201102debf3101881c820a531a5f25
Mariel Torres <108035224+MITdesarrollo@users.noreply.github.com>
2022-11-02 01:05:37 -0300

    Delete src directory

--- src/app/login/models/usuario.ts
+++
@@ -1,5 +0,0 @@
-export interface Usuario{
-  nombre: string;
-  contrasena: string;
-  admin: boolean;
-}
--- src/app/shared/directives/boolean-style.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BooleanStyleDirective } from './boolean-style.directive';
-
-describe('BooleanStyleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BooleanStyleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
